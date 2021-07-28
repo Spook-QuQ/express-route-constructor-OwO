c1496f24e7b1624fd75f373db27c4c4dce508f15
OwO <OwO.futon@gmail.com>
2021-07-28 17:46:41 +0900

    initialize

---
+++ index.js
@@ -0,0 +1,32 @@
+const requireDir = require('require-dir')
+
+const setRouter = (router, routes) => {
+  Object.keys(routes).forEach(routeName => {
+    const routeIndex = routes[routeName].index
+
+    if (!routeIndex) return
+
+    // const routeNameList = Object.keys(routes[routeName]).filter(n => n !== 'index')
+    setRouter(routeIndex.router, routes[routeName])
+
+    const routePath = routeName == 'index' ? // index は root path（ / : スラッシュ ）にする
+        ''
+      :
+        'path' in routeIndex ?  // router に path が無ければ ファイル名 を path にする
+            routeIndex.path
+          :
+            routeName
+
+  // console.log(routePath)
+    if ('middleware' in routeIndex && Array.isArray(routeIndex.middleware) && routeIndex.middleware.length)
+      router.use(`/${ routePath }`, ...routeIndex.middleware, routeIndex.router)
+    else
+      router.use(`/${ routePath }`, routeIndex.router)
+  })
+}
+
+module.exports = (router, dirPath) => {
+  const routes = requireDir(module.parent.path + dirPath || '/', { recurse: true })
+
+  setRouter(router, routes)
+}
---
+++ sample/index.js
@@ -0,0 +1,40 @@
+/*
+  * http://localhost:3000/sample_desu/hoge/tsuna
+  */
+
+var express = require('express');
+var router = express.Router({ mergeParams: true });
+// margeParams: true がないと、親routerのparamsを子routerからアクセスできない
+
+const path = 'sample_desu/:shop' // path が無い場合フォルダ名がルート名になる
+// この場合で path がなかった時、フォルダ名である sample がルート名になる
+
+/* GET users listing. */
+router.get('/:sushi', function(req, res, next) {
+
+  console.log(req.params)
+  const { shop, sushi } = req.params
+  res.send(`
+    <span>
+      <p>respond with a</p>
+      <u><b>${ shop ? shop + "'s" : 'anonymas' }</b></u>
+      <br />
+      <u><b>${ sushi ? sushi + ' sushi' : 'nothing' }</b></u>
+    </span>
+  `)
+})
+
+module.exports = {
+  router,
+  path, // なければapp.js上で path はファイル名になる
+  middleware: [
+    (req, res, next) => {
+      console.log('test middleware 1')
+      next()
+    },
+    (req, res, next) => {
+      console.log('test middleware 2')
+      next()
+    }
+  ]
+}
